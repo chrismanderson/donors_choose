4d55b529a162b20649ed3acc80d2052babb64422
Chris Anderson <christopher.anderson@gmail.com>
2012-07-11 20:46:39 -0400

    URL parsing

--- lib/donors_choose/project.rb
+++ lib/donors_choose/project.rb
@@ -1,7 +1,6 @@
 # This API allows us to find information about DonorsChoose projects,
 # so a Project is the main class of the gem.
 class DonorsChoose::Project
-
   # This method finds all projects near a given latitude and longitude.
   def self.near_me(latitude, longitude)
     DonorsChoose::Request.get(:centerLat => latitude, :centerLong => longitude)
@@ -17,10 +16,18 @@ class DonorsChoose::Project
   end
 
   def self.by_url(url)
-    DonorsChoose::Request.get(:id => parse_id_from_url(url))
+    if donors_url? url
+      DonorsChoose::Request.get(:id => parse_id_from_url(url))
+    else
+      nil
+    end
   end
 
-  def self.parse_id_from_url url
+  def self.donors_url? url
+    url.include? 'donorschoose.org/project/'
+  end
+
+  def self.parse_id_from_url(url)
     url.split('/').last
   end
 end
